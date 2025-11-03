afdb849ac699d4f40eb2a18a2dee0aee6a5b9ebb
Chris Hatch <devcebfaff@proton.me>
2025-11-03 11:13:56 +1000

    update 0011_topup_preferences.sql with new payment timing column and type

--- sqitch/deploy/0011_topup_preferences.sql
+++ sqitch/deploy/0011_topup_preferences.sql
@@ -2,7 +2,9 @@
 
 BEGIN;
 
-CREATE TYPE myenergy.balance_strategy AS ENUM ('simple', 'smooth');
+CREATE TYPE myenergy.balance_strategy_enum AS ENUM ('simple', 'smooth');
+
+CREATE TYPE myenergy.payment_timing_enum AS ENUM ('monthly', 'weekly');
 
 ALTER TABLE myenergy.wallets
     RENAME COLUMN topup_threshold TO target_balance;
@@ -11,12 +13,18 @@ ALTER TABLE myenergy.wallets
     RENAME COLUMN topup_amount TO minimum_balance;
 
 ALTER TABLE myenergy.wallets
-    ADD COLUMN balance_enum myenergy.balance_strategy;
+    ADD COLUMN balance_enum myenergy.balance_strategy_enum;
+
+ALTER TABLE myenergy.wallets
+    ADD COLUMN payment_timing myenergy.payment_timing_enum;
 
 ALTER TABLE myenergy.wallets
     ALTER COLUMN target_balance SET DEFAULT 30;
 
 ALTER TABLE myenergy.wallets
     ALTER COLUMN minimum_balance SET DEFAULT 20;
+
+ALTER TABLE myenergy.wallets
+    ALTER COLUMN payment_timing SET DEFAULT 'monthly';
 
 COMMIT;
--- sqitch/revert/0011_topup_preferences.sql
+++ sqitch/revert/0011_topup_preferences.sql
@@ -6,11 +6,16 @@ ALTER TABLE myenergy.wallets
     DROP COLUMN balance_enum;
 
 ALTER TABLE myenergy.wallets
+    DROP COLUMN payment_timing;
+
+ALTER TABLE myenergy.wallets
     RENAME COLUMN target_balance TO topup_threshold;
 
 ALTER TABLE myenergy.wallets
     RENAME COLUMN minimum_balance TO topup_amount;
 
-DROP TYPE myenergy.balance_strategy;
+DROP TYPE myenergy.balance_strategy_enum;
+
+DROP TYPE myenergy.payment_timing_enum;
 
 COMMIT;
