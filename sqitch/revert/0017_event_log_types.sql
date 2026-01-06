05cd76b6db2fca93bab2c17cc321ae902d4e454a
Chris Hatch <devcebfaff@proton.me>
2026-01-06 12:23:18 +1000

    add event_log_types metadata

---
+++ sqitch/revert/0017_event_log_types.sql
@@ -0,0 +1,7 @@
+-- Revert supabase:0017_event_log_types from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ sqitch/verify/0017_event_log_types.sql
@@ -0,0 +1,7 @@
+-- Verify supabase:0017_event_log_types on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
