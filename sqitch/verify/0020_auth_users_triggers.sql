9df93c9c4b5763f51943379e7f8223f48f3e22ef
Chris Hatch <devcebfaff@proton.me>
2026-05-05 06:01:33 +1000

    remove unused verify script - should have been removed when all merged into 0019

--- sqitch/verify/0020_auth_users_triggers.sql
+++
@@ -1,9 +0,0 @@
--- Verify supabase:0020_auth_users_triggers on pg
-
-BEGIN;
-
-SELECT 1 FROM pg_trigger WHERE tgname = 'update_customers_on_email_update_trigger';
-SELECT 1 FROM pg_trigger WHERE tgname = 'customer_registration_trigger';
-SELECT 1 FROM pg_trigger WHERE tgname = 'customer_status_auth_users_update';
-
-ROLLBACK;
